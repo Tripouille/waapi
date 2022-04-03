78304590f23e00e4fec8a8d4c9fa37a3e071a470
Tripouille <jmichel.gambard@gmail.com>
2022-04-03 17:44:46 +0200

    feat(header): improve semantic with nav

--- components/Layout/Header/StandardHeader.tsx
+++ components/Layout/Header/StandardHeader.tsx
@@ -7,7 +7,7 @@ const StandardHeader = () => {
     <Box px="5%" py={5} bgColor="white">
       <Flex align="center">
         <Logo mr={10} />
-        <Flex align="center" justify="space-between" flex={1}>
+        <Flex as="nav" align="center" justify="space-between" flex={1}>
           <HeaderActions />
         </Flex>
       </Flex>
