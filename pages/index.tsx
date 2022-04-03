630cd71a7c9f463647fcb5455391f681ef6dfec1
Tripouille <jmichel.gambard@gmail.com>
2022-04-03 11:15:40 +0200

    feat(home-page): add welcome and  search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import { Flex, Heading } from '@chakra-ui/react';
 import ProductsGrid from 'components/ProductsGrid';
+import SearchBar from 'components/SearchBar';
 import type { NextPage } from 'next';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+  const [searchTerms, setSearchTerms] = useState('');
+
   return (
     <div>
-      <ProductsGrid mt={10} />
+      <Flex justify="center" alignItems="center" mt={['4', '10']} flexDirection={['column', 'row']}>
+        <Heading as="h1" mr={['0', '16']} mb={['4', '0']}>
+          Welcome!
+        </Heading>
+        <SearchBar onSearch={setSearchTerms} placeholder="Search a product" />
+      </Flex>
+      <ProductsGrid mt={10} searchTerms={searchTerms} />
     </div>
   );
 };
