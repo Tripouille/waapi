4c0d603154bea65662a952b986aafac928ea73ee
Tripouille <jmichel.gambard@gmail.com>
2022-04-04 13:04:55 +0200

    refactor(product-form): create CreateProductForm component

--- pages/products/create.tsx
+++ pages/products/create.tsx
@@ -1,20 +1,7 @@
-import ProductForm from 'components/ProductForm';
-import { BsPlusLg } from 'react-icons/bs';
-import { useCreateProductMutation } from 'services/product';
+import CreateProductForm from 'components/CreateProductForm';
 
 const CreateProductPage = () => {
-  const { mutate: createProduct, isLoading, isSuccess } = useCreateProductMutation();
-
-  return (
-    <ProductForm
-      title="Add a new product"
-      titleIcon={BsPlusLg}
-      submitLabel="Add a new product"
-      onSubmit={createProduct}
-      isLoading={isLoading}
-      isSuccess={isSuccess}
-    />
-  );
+  return <CreateProductForm />;
 };
 
 export default CreateProductPage;
