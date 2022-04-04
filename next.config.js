42f87eface5835500b2d605c2933b9fc1895ddca
Tripouille <jmichel.gambard@gmail.com>
2022-04-04 17:07:29 +0200

    fix(i18n): fix missing lang in html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['fr-FR'],
+    defaultLocale: 'fr-FR',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
